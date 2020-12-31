ca09347f4cb0e9c66823d56da09974007c6b80c1
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 08:20:45 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,25 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      :patriarch => {:name => 'Lord Montague', 
-                    :age => '53'},
-      :matriarch => {:name => 'Lady Montague', 
-                    :age => '54'},
-      :hero => {:name => 'Romeo', 
-                :age => '15', 
-                :status => 'alive'},
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      :patriarch => {:name => 'Lord Capulet', 
-                    :age => '50'},
-      :matriarch => {:name => 'Lady Capulet', 
-                    :age => '51'},
-      :hero => {:name => 'Juilet', 
-                :age => '15', 
-                :status => 'alive'},
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }
     
